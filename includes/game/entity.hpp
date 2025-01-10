66be683f3774e6a7c32e15034ef66948f3a16588
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-10 13:28:43 +0100

    started writing entity class and added .vscode folder to gitignore

--- includes/game/entity.hpp
+++ includes/game/entity.hpp
@@ -1,6 +1,17 @@
 #ifndef ENTITY_HPP
 #define ENTITY_HPP
 
+#include <string>
 
+class Entity
+{
+    double cordX, cordY, cordZ;
+    float* vertices;
+    std::string vertexShader;
+    std::string fragmentShader;
+
+    Entity();
+    virtual ~Entity();
+};
 
 #endif
