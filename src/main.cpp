00f116e1bdafb698c029e0cec63acdf1af42a72d
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-28 18:35:56 +0100

    wrote main for windows api

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,7 @@ void operator delete[](void* ptr, size_t size) noexcept {
 
 #endif
 
+
 int main(int argc, char** argv){
     Engine e(argc, argv);
 
@@ -55,3 +56,47 @@ int main(int argc, char** argv){
     #endif
     return 0;
 }
+
+
+#ifdef _WIN32
+// Windows-specific WinMain entry point
+int WINAPI WinMain(
+    HINSTANCE hInstance,      // Handle to the current instance of the application
+    HINSTANCE hPrevInstance,  // Handle to the previous instance (always NULL in modern Windows)
+    LPSTR lpCmdLine,          // Command-line arguments as a single string
+    int nCmdShow              // Controls how the window is to be shown
+) {
+    // Parse command-line arguments into argc and argv
+    int argc = 0;
+    char** argv = nullptr;
+
+    LPSTR* argvWide = CommandLineToArgvW(GetCommandLineW(), &argc);
+    if (argvWide) {
+        argv = new char*[argc];
+        for (int i = 0; i < argc; i++) {
+            size_t size = wcslen(argvWide[i]) + 1;
+            argv[i] = new char[size];
+            wcstombs(argv[i], argvWide[i], size);
+        }
+        LocalFree(argvWide);
+    }
+
+    // Instantiate Engine with command-line arguments
+    Engine e(argc, argv);
+
+    // Free allocated argv memory
+    for (int i = 0; i < argc; i++) {
+        delete[] argv[i];
+    }
+    delete[] argv;
+
+    // Keep track of allocated memory and freed memory
+    #ifndef NDEBUG
+    std::string debugMessage = "unfreed memory in bytes: " + std::to_string(allocatedBytes) + "\n" +
+                                "allocations that didn't get freed: " + std::to_string(countAllocation) + "\n";
+    OutputDebugString(debugMessage.c_str());
+    #endif
+
+    return 0;
+}
+#endif
